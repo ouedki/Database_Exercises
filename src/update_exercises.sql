63636de1c34a321b7b67ae15cda47a4e58811a14
ouedki <yassineouedki_5@hotmail.com>
2017-01-25 09:18:15 -0600

    Mysql CRUD exercises

--- src/update_exercises.sql
+++ src/update_exercises.sql
@@ -1,10 +1,8 @@
 USE codeup_test_db;
 
-SELECT name
-FROM albums;
+SELECT * FROM albums;
 
-SELECT name
-FROM albums
+SELECT * FROM albums
 WHERE release_date<1980;
 
 SELECT name
@@ -16,21 +14,11 @@ SET sales = sales * 10;
 
 UPDATE albums
 SET release_date = 1800
-WHERE release_date BETWEEN 1800 AND 1980;
+WHERE release_date < 1980;
 
 UPDATE albums
 SET artist = 'Peter Jackson'
 WHERE artist = 'Michael Jackson';
 
-SELECT name, sales
-FROM albums
-WHERE sales >20000000;
-
-SELECT name, release_date
-FROM albums
-WHERE release_date = 1800;
-
-SELECT artist
-FROM albums
-WHERE artist = 'Peter Jackson';
+SELECT * FROM albums;
 
